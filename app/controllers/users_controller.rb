7fbc15bba8c343115a359071a3ab584469e1a768
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-09-14 12:19:54 -0400

    user in database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :email, presence: true, length: { maximum: 30 }
+  has_secure_password
+  scope :sorted, -> { order('id ASC') }
+  scope :newest_first, -> { order('created_at DESC') }
+
+  def full_name
+    first_name + ' ' + last_name
+  end
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def authenicated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 end
