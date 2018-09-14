e6905116806213a76b7a24473658a8ed662a28e0
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2018-09-14 14:51:04 -0400

    solved more merge conflicts

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -4,9 +4,7 @@ class Gym < ApplicationRecord
   has_many :events
 
   has_one_attached :cover_photo
-  # has_many :events
-
-  # belongs_to :user
+  belongs_to :user
     before_save :default_values
     before_save { self.user_id = user_id.to_i }
   validates :name, :location, :user_id, presence: true
