194bc54aaaca7d1366e902789c1235dade7b6eb8
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-09-17 11:27:19 -0400

    fixed the register photo problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 8 }
   validates :email, presence: true, length: { maximum: 30 }
   has_secure_password
+   has_one_attached :photo
   scope :sorted, -> { order('id ASC') }
   scope :newest_first, -> { order('created_at DESC') }
 
