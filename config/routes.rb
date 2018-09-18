a24606b45979803774aacabe6a93ffa75ef0dd3e
Milo Wissig <mwissig@gmail.com>
2018-09-18 13:27:52 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,22 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
 
-  get 'messages/index'
-  get 'messages/new'
-  get 'messages/edit'
-  get 'messages/show'
- root  'pages#home'
-=======
   root 'pages#home'
->>>>>>> 7fad244ebdec0da1c1d985225fdb30a4907f6f1d
-  get 'participants/index'
-  get 'participants/edit'
-  get 'participants/show'
-  get 'participants/new'
 
-  resources :users, only: %i[edit index create show update destroy]
+  resources :users, only: %i[edit index create show update destroy] do
+      resources :messages
+  end
+
   resources :gyms do
     resources :events do
       resources :participants do
         delete 'delete' => 'participants#destroy'
       end
-<<<<<<< HEAD
-end
-end
-
-resources :users do
-  resources :messages
-end
-
-=======
     end
-  end
->>>>>>> 7fad244ebdec0da1c1d985225fdb30a4907f6f1d
+end
+
 
   get '/results' => 'pages#results'
   get '/gyms' => 'gyms#index'
-
-  get 'gyms/show'
-  get 'gyms/edit'
-  get 'gyms/new'
-
 
   get 'register' => 'users#new'
 
