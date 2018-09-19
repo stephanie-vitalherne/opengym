8a19a299e02797623b26070f518c716948fe281b
Milo Wissig <mwissig@gmail.com>
2018-09-19 11:13:53 -0400

    distance tests

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,26 +18,86 @@
 
 
 function menuReveal() {
-    document.getElementById("bar-container").classList.toggle("change");
-    document.getElementById("nav-box").classList.toggle("pop-up");
-    document.getElementById("yield-container").classList.toggle("hidden");
+  document.getElementById("bar-container").classList.toggle("change");
+  document.getElementById("nav-box").classList.toggle("pop-up");
+  document.getElementById("yield-container").classList.toggle("hidden");
 }
 
 function search() {
-    // document.getElementById("search-bar").classList.toggle("hidden");
-    // document.getElementById("search-bar").classList.toggle("slider");
-    // document.getElementById("nav-top-logo").classList.toggle("hidden");
-    // document.getElementById("cancel").classList.toggle("hidden");
-    // document.getElementById("go").classList.toggle("hidden");
-    // document.getElementById("search-icon").classList.toggle("hidden");
-    document.getElementById("search").classList.toggle("search-transition");
-    // document.getElementById("og-top").classList.toggle("right");
+  // document.getElementById("search-bar").classList.toggle("hidden");
+  // document.getElementById("search-bar").classList.toggle("slider");
+  // document.getElementById("nav-top-logo").classList.toggle("hidden");
+  // document.getElementById("cancel").classList.toggle("hidden");
+  // document.getElementById("go").classList.toggle("hidden");
+  // document.getElementById("search-icon").classList.toggle("hidden");
+  document.getElementById("search").classList.toggle("search-transition");
+  // document.getElementById("og-top").classList.toggle("right");
 }
 
 function newGymOpen() {
-      document.getElementById("newmodal").classList.toggle("open");
+  document.getElementById("newmodal").classList.toggle("open");
 }
 
 function removeShadow() {
-       document.getElementById("footer").classList.remove("shadow");
-     }
+  document.getElementById("footer").classList.remove("shadow");
+}
+
+
+let allCoords = {};
+
+let getBothCoordPairs = (address) => {
+
+navigator.geolocation.getCurrentPosition((position) => {
+
+    let lat = position.coords.latitude;
+    let lng = position.coords.longitude;
+
+    console.log(`LATITUDE: ${lat} LONGITUDE ${lng}`);
+
+    allCoords.userLoc = {latitude: lat, longitude: lng};
+
+    retrieveLocationDataAddressToCoords(address);
+
+});
+
+}
+
+let retrieveLocationDataAddressToCoords = (addressArg) => {
+console.log(addressArg);
+
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addressArg}&components=country:US&key=AIzaSyDuEG7OD_U36SiWbM7HkJkom9cbzXkaq04`)
+.then((response) => {
+    console.log(response);
+
+    allCoords.endLoc = {latitude: response.data.results[0].geometry.location.lat, longitude: response.data.results[0].geometry.location.lng};
+    console.log(allCoords);
+
+    document.getElementById("myLoc").value += `Lat: ${allCoords.userLoc.latitude} Lng: ${allCoords.userLoc.longitude}`;
+    document.getElementById("endLoc").value += `Lat: ${allCoords.endLoc.latitude} Lng: ${allCoords.endLoc.longitude}`;
+
+
+    calculateDistance();
+}).catch((error) => {
+    console.log(error);
+});
+}
+
+let calculateDistance = () => {
+    let calculate = new google.maps.DistanceMatrixService();
+
+    calculate.getDistanceMatrix(
+        {
+          origins: [{lat: allCoords.userLoc.latitude, lng: allCoords.userLoc.longitude}],
+          destinations: [{lat: allCoords.endLoc.latitude, lng: allCoords.endLoc.longitude}],
+          unitSystem: google.maps.UnitSystem.IMPERIAL,
+          travelMode: 'DRIVING' //<---Default
+        },
+        (response, status) => {
+            console.log(response);
+            var s = response.rows[0].elements[0].distance.text.replace(' mi', '');
+            document.getElementById("dist").value += s;
+            console.log(status);
+        }
+    );
+
+}
