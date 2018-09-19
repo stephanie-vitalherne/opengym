4ac45a7262467ab93851511036ef6af8475fdaff
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-09-19 12:47:11 -0400

    conditional added for the user name length

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,36 +1,24 @@
 class PagesController < ApplicationController
+  def home; end
 
+  def search; end
 
+  def results
+    if @results = params[:q].capitalize
+      # @gym = Gym.where('name LIKE ?', "%#{@results}%")
+      @event = Event.where('name LIKE ? or event_type LIKE ?', "%#{@results}%", "%#{@results}%")
+      @gym = Gym.find_by(params[:gym_id])
+    else
 
-
-
-  def home
+      @event = Event.all
   end
-
-def search
-end
-
-def results
-
-  if @results = (params[:q]).capitalize
-  # @gym = Gym.where('name LIKE ?', "%#{@results}%")
-  @event = Event.where('name LIKE ? or event_type LIKE ?', "%#{@results}%", "%#{@results}%")
-    @gym = Gym.find_by(params[:gym_id])
-else
-
-  @event = Event.all
-end
-
-  end
-
+        end
 
   def leaderboard
     @users = User.all.order(points: :desc)
    end
 
-
   def basketball; end
-
 
   def football; end
 
