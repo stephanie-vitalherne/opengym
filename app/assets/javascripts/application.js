b9e5b5aa34b0342100ef8c5a93fbe17c82326ef5
Milo Wissig <mwissig@gmail.com>
2018-09-20 10:58:44 -0400

    distance works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,13 +20,7 @@
 function menuReveal() {
   document.getElementById("bar-container").classList.toggle("change");
   document.getElementById("nav-box").classList.toggle("pop-up");
-//   if (document.getElementById("yield-container").style.display = 'block') {
-//         document.getElementById("yield-container").style.display = 'none';
-// }if(document.getElementById("yield-container").style.display = 'none'){
-//     setTimeout(function(){
-//         document.getElementById("yield-container").style.display="block";
-//     }, 500);
-//   }
+  document.getElementById("yield-container").classList.toggle("hidden");
 }
 
 function search() {
@@ -58,34 +52,33 @@ navigator.geolocation.getCurrentPosition((position) => {
 
     allCoords.userLoc = {latitude: lat, longitude: lng};
     // document.getElementById("location_coordinates").value = allCoords.userLoc.latitude + "," + allCoords.userLoc.longitude;
-    retrieveLocationDataAddressToCoords(address);
+    // retrieveLocationDataAddressToCoords(address);
 
 });
 
 }
 getBothCoordPairs();
-let retrieveLocationDataAddressToCoords = (addressArg) => {
-console.log(addressArg);
-
-axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addressArg}&components=country:US&key=AIzaSyB8hX_GA7hRKU1WEb5pu6HRhmsooFiqQuU`)
-.then((response) => {
-    console.log(response);
 // 
-// axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addressArg}&key=AIzaSyCGZeAC28wRGbVJb__vJjh60ybcwf1EP2Y`)
+// let retrieveLocationDataAddressToCoords = (addressArg) => {
+// console.log(addressArg);
+
+
+//
+// axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addressArg}&key=AIzaSyDuEG7OD_U36SiWbM7HkJkom9cbzXkaq04`)
 // .then((response) => {
 //     console.log(response);
-
-    allCoords.endLoc = {latitude: response.data.results[0].geometry.location.lat, longitude: response.data.results[0].geometry.location.lng};
-    console.log(allCoords);
-
-    // document.getElementById("myLoc").value += `Lat: ${allCoords.userLoc.latitude} Lng: ${allCoords.userLoc.longitude}`;
-    document.getElementById("gym_coordinates").value = allCoords.endLoc.latitude + "," + allCoords.endLoc.longitude;
-
-    // calculateDistance();
-}).catch((error) => {
-    console.log(error);
-});
-}
+//
+//     allCoords.endLoc = {latitude: response.data.results[0].geometry.location.lat, longitude: response.data.results[0].geometry.location.lng};
+//     console.log(allCoords);
+//
+//     // document.getElementById("myLoc").value += `Lat: ${allCoords.userLoc.latitude} Lng: ${allCoords.userLoc.longitude}`;
+//     document.getElementById("gym_coordinates").value = allCoords.endLoc.latitude + "," + allCoords.endLoc.longitude;
+//
+//     // calculateDistance();
+// }).catch((error) => {
+//     console.log(error);
+// });
+// }
 
 // let calculateDistance = () => {
 //     let calculate = new google.maps.DistanceMatrixService();
