ebbdc89b95a82495e9ebf73c58ab6dbf433b5250
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-09-20 02:28:33 -0400

    styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,16 @@
 class PagesController < ApplicationController
+  before_action :find_gym, only: %i[results home basketball football soccer other]
   def search; end
 
   def results
     if @results = params[:q].capitalize
       # @gym = Gym.where('name LIKE ?', "%#{@results}%")
       @event = Event.where('name LIKE ? or event_type LIKE ?', "%#{@results}%", "%#{@results}%")
-      @gym = Gym.find_by(params[:gym_id])
     end
   end
 
   def home
     @event = Event.all
-    @gym = Gym.find_by(params[:gym_id])
   end
 
   def leaderboard
@@ -19,29 +18,26 @@ class PagesController < ApplicationController
   end
 
   def basketball
-
     @basketball = Event.where(event_type: 'Basketball')
-    @gym = Gym.find_by(params[:gym_id])
-
-
-
   end
 
   def football
     @football = Event.where(event_type: 'Football')
-     @gym = Gym.find_by(params[:gym_id])
   end
 
   def soccer
     @soccer = Event.where(event_type: 'Soccer')
-     @gym = Gym.find_by(params[:gym_id])
-   end
+  end
 
   def other
-
     @other = Event.all
-    @gym = Gym.find_by(params[:gym_id])
   end
 
   def about; end
+
+  private
+
+  def find_gym
+    @gym = Gym.find_by(params[:gym_id])
   end
+end
